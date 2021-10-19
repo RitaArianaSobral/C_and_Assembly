d86065e61fd6a060fdc93adcf79c1770e0e66f80
RitaAriana <1201386@isep.ipp.pt>
2021-10-19 22:35:43 +0100

    Exercício 5 - Módulo 1

---
+++ modulo1/ex05/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "upper2.h"
+
+int main(){
+
+    char str [] = "Teste";
+    upper2(str);
+    printf("%s\n",str);
+
+    return 0;
+}
---
+++ modulo1/ex05/upper2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void upper2(char *str){
+
+    while(*str != '\0'){
+        if(*str >= 65 + 32 && *str <= 90 + 32) { 
+            *str = *str - 32;
+        }
+        str++;
+    }
+}

c2c014369e4598b7509490efafe1d5951123725f
FranciscoRedol <1201239@isep.ipp.pt>
2021-10-22 23:48:30 +0100

    Conclusão Ex14

--- modulo1/ex14/frequencies.c
+++ modulo1/ex14/frequencies.c
@@ -1,7 +1,22 @@
-void frequencies(float *grades, int n, int *freq){
-    for(int index = 0; index<n; index++){
+void frequencies(float *grades, int n, int *freq){  
+    int maxNotas = 20;  
+    for(int index = 0; index < n ; index++){
+        int intpart = *grades;
         
-
+        if(intpart >= 0 && intpart <= maxNotas){
+            if(intpart == 0)
+                freq[0] += 1;
+ 
+            else{
+                int i = 1;
+                while(i <= intpart){
+                    if((intpart / i) == 1 && (intpart % i) == 0)
+                        freq[i] += 1;
+                    
+                    i++;
+                }
+            }
+        }
+        grades++;
     }
-
-}
+}  
--- modulo1/ex14/main.c
+++ modulo1/ex14/main.c
@@ -3,15 +3,15 @@
 
 int main(){
 
-    int n = 11;
-    float grades [] = {8.23, 12.25, 16.45, 12.45, 10.05, 6.45, 14.45, 0.0, 12.67, 16.23, 18.75};
-    int freq [20];
+    int n = 10;
+    float grades [] = {20,10,10,10,10,10,10,10,10,10};
+    int freq [] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
 
     frequencies(grades, n, freq);
 
-    for(int i = 0; i<n; i++){
-        printf("%.2f\n", freq);
+    for(int i = 0; i<= 20; i++){
+        printf("%d\n", freq[i]);
     }
 
     return 0;

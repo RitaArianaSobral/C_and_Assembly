83ebe585706baff71b1bbe10149d38cf97a3afd9
RitaAriana <1201386@isep.ipp.pt>
2021-10-23 17:00:20 +0100

    Ex19

--- modulo1/ex19/main.c
+++ modulo1/ex19/main.c
@@ -6,6 +6,9 @@ int main(){
     char word [] = "xrt";
     char str [] = "aarrxrt xrt xrt xrt";
     char *endereco=find_word(word,str);
+    printf("%p\n",str);
+    printf("%c\n",*str);
     printf("%p\n",endereco);
+    printf("%c\n",*endereco);
     return 0;
 }

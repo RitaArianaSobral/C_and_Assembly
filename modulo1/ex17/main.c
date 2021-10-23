8febf0bdbbb03eb4b769a2407bf2e168bd28a9cd
RitaAriana <1201386@isep.ipp.pt>
2021-10-23 11:33:25 +0100

    Exercício 17 - Módulo 1

--- modulo1/ex17/main.c
+++ modulo1/ex17/main.c
@@ -3,5 +3,14 @@
 
 int main(){
 
+    int vec1 []={1,2,3,4,9};
+    int vec2 []={9,8,7,6,1};
+    swap(vec1,vec2,5);
+    for (int i=0;i<5;i++){
+        printf("%d\n",*(vec1+i));
+    }
+    for (int i=0;i<5;i++){
+        printf("%d\n",*(vec2+i));
+    }
     return 0;
 }

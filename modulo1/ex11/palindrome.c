f82a9996eefc1a0777c2ecbe48849b81c6667644
RitaAriana <1201386@isep.ipp.pt>
2021-10-23 21:59:44 +0100

    Exercício 11 - Módulo 1

--- modulo1/ex11/palindrome.c
+++ modulo1/ex11/palindrome.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
+
 int palindrome(char *str){
     int lenght=0;
     
     for(int i=0;*(str+i) != '\0';i++){
         lenght++;
     }
+
     char aux [lenght];
     for(int i=0;i<=lenght;i++){
         *(aux+i)='\0';
     }
 
     int j=0;
-    for(int i=0;i<=lenght;i++){
-        if ((*(str+i) >= 'A' && *(str+i) <= 'Z') || (*(str+i) >= 'a' && *(str+i) <= 'z') || (*(str+i) >= '1' && *(str+i) <= '9')){
+    for(int i=0;i<lenght;i++){
+        if ((*(str+i) >= 'A' && *(str+i) <= 'Z') || (*(str+i) >= 'a' && *(str+i) <= 'z') || (*(str+i) >= '0' && *(str+i) <= '9')){
             *(aux+j)=*(str+i);
             j++;
         }
@@ -24,9 +25,20 @@ int palindrome(char *str){
     }
     
     int h=temp-1;
-    for(int m=0;m<temp;m++){
-        if(*(aux+m) == *(aux+temp)){
+
+    for(int i=0;i<temp;i++){
+        if(*(aux+i) >= 65 + 32 && *(aux+i) <= 90 + 32) { 
+            *(aux+i) = *(aux+i) - 32;
+        }
+    }
+
+    for(int m=0;m<=temp;m++){
+        if(temp>0){
+            if(*(aux+m) == *(aux+h)){
             h--;
+            } else {
+            return 0;
+            }
         } else {
             return 0;
         }

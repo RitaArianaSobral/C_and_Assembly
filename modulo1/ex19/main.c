9f35eda5e64a7e2eee812d966ef814ea7ed25651
RitaAriana <1201386@isep.ipp.pt>
2021-10-27 16:08:36 +0100

    Finalização do exercício 19

--- modulo1/ex19/main.c
+++ modulo1/ex19/main.c
@@ -6,10 +6,7 @@ int main(){
     char word [] = "x";
     char str [] = "rrxXXXX";
 
-    char *endereco=find_word(word,str);
-    printf("%p\n",str);
-    printf("%c\n",*str);
-    printf("%p\n",endereco);
-    printf("%c\n",*endereco);
+    find_word(word,str);
+    
     return 0;
 }

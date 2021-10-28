b167c91cfafacb74f88955131a18b4029244d40d
FranciscoRedol <1201239@isep.ipp.pt>
2021-10-28 10:51:52 +0100

    Conclusão do Exercício 20

--- modulo1/ex20/find_all_words.c
+++ modulo1/ex20/find_all_words.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
+
 void find_all_words(char* str, char* word, char** addrs){
-    int existe=0;
-    
+    int existe = 0;
+    int index = 0;
     
     while(existe==0 && *(str) != '\0'){
         int valid=0;
@@ -21,11 +23,17 @@ void find_all_words(char* str, char* word, char** addrs){
             existe=0;
         }
         if(existe == 1){
-            *addrs = str;
-            addrs++;
+            (*(addrs + index)) = str;
+            index++;
             existe = 0;
             valid = 0;            
         }
         str++;
+    }
+
+    if(index != 0){
+        printf("A palavra pretendida encontra-se nos seguintes endereços:\n");
+        for(int i = 0; i<index; i++)
+            printf("%p\n", *(addrs+i));
     }
 }

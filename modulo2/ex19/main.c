7f1289a8d8a2dd534cfc2378ad386d60ff9fc6f0
RitaAriana <1201386@isep.ipp.pt>
2021-11-22 18:05:05 +0000

    Exercício 19 - Módulo 2 : passa em todos os testes

---
+++ modulo2/ex19/main.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "asm.h"
+
+int current = -2;
+int desired = -1;
+int main(){
+
+    result = needed_time();
+    printf("\nTempo que demora: %d", result);
+
+    return 0;
+}
